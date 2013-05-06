50062de95f197ab0b4200c8a7f90fe5f184fdb2d
Andrew Ritchie <ritchiea@gmail.com>
2013-05-06 07:49:01 -0400

    cleanup

--- lib/april/querybuilder.rb
+++ lib/april/querybuilder.rb
@@ -12,16 +12,13 @@ module April
       private
 
       def reporting_period(config)
-        result = '' 
-        result += "#{config[:date_column]} >= '#{config[:start_date]}' AND "
+        result = "#{config[:date_column]} >= '#{config[:start_date]}' AND "
         result += "#{config[:date_column]} <= '#{config[:end_date]}'"
         result
       end
 
       def append_conditions(config)
-        result = ''
-        result += " AND #{config[:conditions]} "
-        result
+        " AND #{config[:conditions]} "
       end
 
     end
