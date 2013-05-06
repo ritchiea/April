503f05fcb6ed91e397335606437579a6a56812fe
Andrew Ritchie <ritchiea@gmail.com>
2013-05-05 22:46:03 -0400

    I think this should work on build

--- april.gemspec
+++ april.gemspec
@@ -1,11 +1,14 @@
 Gem::Specification.new do |s|
-  s.name = %q{April}
+  s.name = %q{april}
   s.version = "0.1.0"
   s.date = %q{2013-05-05}
   s.summary = %q{April is dead simple reporting for Rails}
+  s.authors = ["Andrew Ritchie"]
+  s.email = ["hello@andrewritchie.info"]
+  s.homepage = "https://github.com/ritchiea/April"
   s.files = [
     "lib/april.rb",
-    "lib/querybuilder.rb"
+    "lib/april/querybuilder.rb"
   ]
   s.require_paths = ["lib"]
 end
--- lib/april/querybuilder.rb
+++ lib/april/querybuilder.rb
@@ -1,11 +1,8 @@
 module April
   class Querybuilder
 
-    DEFAULTS = {}
-
     def self.build(config)
       query = reporting_period config
-      query += self.send("build_#{config[:klass]}".downcase.to_sym, config) if DEFAULTS.keys.include? config[:klass].to_s.downcase.to_sym
       query += append_conditions config if config[:conditions].present?
       query
     end
@@ -25,14 +22,6 @@ module April
         result
       end
 
-      DEFAULTS.each do |k,v|
-        define_method "build_#{k}".to_sym do |*args|
-          config = args[0]
-          result = DEFAULTS[k.to_sym]
-          result += " AND #{config[:value_column]} > 0" if config[:type] == 'sum'
-          result
-        end
-      end
     end
 
   end
